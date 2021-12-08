a8621d79ebaa5bd3f5bcb1e2eb2537be3fc78ac6
Alex <alex.chart@turningpointhr.com>
2021-12-08 16:08:26 +0000

    commit tree 77197e40fbed92311c22714c1157a3eee333af34

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(function(driver)
+    {
+       return driver.toLowerCase() === string.toLowerCase()
+    })
+ }
+
+function fuzzyMatch(array, string){
+    return array.filter(function(element) {
+        return element.startsWith(string) })
+}
+
+function matchName (drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name === name
+    })
+}
